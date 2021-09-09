5d41f5310fd7715444258c02466c1c5e6cd49987
Nicole Arruda <nicole.arruda@accenture.com>
2021-09-09 17:03:28 -0400

    Make Elixir test a little more realistic
    The list of items passed to `update_quality` will not be updated in place. Changing the test to instead assert on the list returned from the function.

--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -3,8 +3,7 @@ defmodule GildedRoseTest do
 
   test "begin the journey of refactoring" do
     items = [%Item{name: "foo", sell_in: 0, quality: 0}]
-    GildedRose.update_quality(items)
-    %{name: firstItemName} = List.first(items)
+    %{name: firstItemName} = GildedRose.update_quality(items) |> List.first()
     assert "fixme" == firstItemName
   end
 end
