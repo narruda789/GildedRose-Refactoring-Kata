a65057361576bbc9f6114804d7c89c11784c23e6
Nicole Arruda <nicole.arruda@accenture.com>
2021-09-09 16:47:30 -0400

    Add base case characterization tests

--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -1,10 +1,17 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "begin the journey of refactoring" do
-    items = [%Item{name: "foo", sell_in: 0, quality: 0}]
-    GildedRose.update_quality(items)
-    %{name: firstItemName} = List.first(items)
-    assert "fixme" == firstItemName
+  describe "update_quality" do
+    test "lowers quality for every item" do
+      items = [%Item{name: "Cheez-Its", sell_in: 0, quality: 10}]
+      %{quality: cheezitQuality} = GildedRose.update_quality(items) |> List.first()
+      assert cheezitQuality == 8
+    end
+
+    test "lowers sell_in for every item" do
+      items = [%Item{name: "Cheez-Its", sell_in: 14, quality: 0}]
+      %{sell_in: cheezitSellIn} = GildedRose.update_quality(items) |> List.first()
+      assert cheezitSellIn == 13
+    end
   end
 end
